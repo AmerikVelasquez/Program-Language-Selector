06932c7877f9daea52fb58ee15a6ba2d020e8e99
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-06-20 01:05:13 -0700

    add readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
   $("form#questions").submit(function(event){
     event.preventDefault();
-    const preference= parseInt($("#preference").val());
-    const interest= parseInt($("#interests").val());
-    const satisfaction= parseInt($("#satisfaction").val());
-    const transportation= parseInt($("#transportation").val());
-    const attachments= parseInt($("#attachments").val());
-    const add= parseInt(preference+interest+satisfaction+transportation+attachments).val();
-  
-    if ("add">13){
-      let result= "swift";
-    } else if ("add"<7){
-      let result= "java";
+    const preference = parseInt($("#pref").val());
+    const interest = parseInt($("#interests").val());
+    const satisfaction = parseInt($("#satisfaction").val());
+    const transportation = parseInt($("#transportation").val());
+    const attachments = parseInt($("#attachments").val());
+    const total = (preference + interest + satisfaction + transportation + attachments).val();
+    
+    
+    if (total>13) {
+      $("#result").text("swift");
+      $("#closing").show();
+    } else if (total < 5){
+      $("#result").text("javaScript");
+      $("#closing").show();
     } else {
-      let result= "c-Sharp";
+      $("#result").text("cSharp");
+      $("#closing").show();
     }
-    return result;
-  }
-}
+    
+  });
+});
