48cd8a86b33df491a0f75583686f6a76118c0a0e
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-06-23 21:32:20 -0700

    fix the user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-  $("form#questions").submit(function(event){
+  $("#questions").submit(function(event){
     event.preventDefault();
     const preference = parseInt($("#pref").val());
     const interest = parseInt($("#interests").val());
     const satisfaction = parseInt($("#satisfaction").val());
     const transportation = parseInt($("#transportation").val());
     const attachments = parseInt($("#attachments").val());
-    const total = (preference + interest + satisfaction + transportation + attachments).val();
+    const total = (preference + interest + satisfaction + transportation + attachments);
     
     
-    if (total>13) {
+    if (total >12) {
       $("#result").text("swift");
       $("#closing").show();
-    } else if (total < 5){
+    } else if (total < 7){
       $("#result").text("javaScript");
       $("#closing").show();
     } else {
